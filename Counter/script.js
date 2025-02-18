9e77ddeb89cac1de2735e91939f8c48ab59edd46
ppratibha53 <108935815+ppratibha53@users.noreply.github.com>
2025-02-18 12:54:42 +0530

    counter

--- Counter/script.js
+++ Counter/script.js
@@ -1,6 +1,7 @@
 let CounterDisplayElm = document.querySelector('.counter-display');
 let CounterMinusElem = document.querySelector('.counter-minus');
 let CounterPlusElem = document.querySelector('.counter-plus');
+let Counterreset = document.querySelector('.counter-reset');
 
 
 let count = 0;
@@ -10,16 +11,31 @@ updateDisplay();
 CounterPlusElem.addEventListener('click',()=>{
  count ++;
  updateDisplay();
- changeBackgroundColor()
+ changeBackgroundColor();
 });
 
 CounterMinusElem.addEventListener('click',()=>{
     count --;
     updateDisplay();
-    changeBackgroundColor()
+    changeBackgroundColor();
 });
+Counterreset.addEventListener('click', ()=>{
+    CounterDisplayElm.innerHTML  = 0
+})
 function updateDisplay(){
     CounterDisplayElm.innerHTML = count;
 };
+
+
+
+function changeBackgroundColor(){
+    if (CounterDisplayElm.innerHTML>0){
+        CounterDisplayElm.style.color = "green";
+
+    } else if(CounterDisplayElm.innerHTML<0){
+        CounterDisplayElm.style.color = " red";
+    } 
+  
+}
 
 
